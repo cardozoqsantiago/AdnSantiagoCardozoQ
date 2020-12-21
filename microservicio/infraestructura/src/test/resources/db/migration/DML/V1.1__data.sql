75b26389b1d6318dd9170fd13d316cc88d23a0df
Santiago Cardozo Quitian - Ceiba Software <santiago.cardozo@ceiba.com.co>
2020-12-21 13:53:26 -0500

    cambio en bd memoria

--- microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
+++ microservicio/infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
@@ -1,2 +1,1 @@
-insert into producto(nombre,tipo_producto,cantidad,precio) values('test','BLUSA',10,50000);
-insert into reserva(fecha,producto_id) values('2020-12-21',1);
+insert into producto(nombre,tipo_producto,cantidad,precio) values('test','BLUSA',10,50000)
