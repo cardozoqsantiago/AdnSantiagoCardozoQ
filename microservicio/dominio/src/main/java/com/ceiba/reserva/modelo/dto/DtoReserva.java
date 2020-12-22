fc485618ab9731a5f057b6081c568798967982ad
Santiago Cardozo Quitian - Ceiba Software <santiago.cardozo@ceiba.com.co>
2020-12-22 09:37:02 -0500

    cambio en constructor dtoreserva

--- microservicio/dominio/src/main/java/com/ceiba/reserva/modelo/dto/DtoReserva.java
+++ microservicio/dominio/src/main/java/com/ceiba/reserva/modelo/dto/DtoReserva.java
@@ -1,19 +1,28 @@
 package com.ceiba.reserva.modelo.dto;
 
 import java.time.LocalDate;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class DtoReserva {
-	
-	 	private Long id;
-	    private LocalDate fechaReserva;
-	    private Long idProducto;
-	    private String nombre;
-	    private String tipoProducto;
-	    private Long cantidad;
-	    private Double precio;
+
+	private Long id;
+	private LocalDate fechaReserva;
+	private Long idProducto;
+	private String nombre;
+	private String tipoProducto;
+	private Long cantidad;
+	private Double precio;
+
+	public DtoReserva(Long id, LocalDate fechaReserva, Long idProducto, String nombre, String tipoProducto,
+			Long cantidad, Double precio) {
+		this.id = id;
+		this.fechaReserva = fechaReserva;
+		this.idProducto = idProducto;
+		this.nombre = nombre;
+		this.tipoProducto = tipoProducto;
+		this.cantidad = cantidad;
+		this.precio = precio;
+	}
 
 }
