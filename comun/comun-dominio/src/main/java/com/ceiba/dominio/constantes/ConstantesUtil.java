754aacf2b0e91046bfe269c5c291872216b8fd9c
Santiago Cardozo Quitian - Ceiba Software <santiago.cardozo@ceiba.com.co>
2020-12-23 08:24:46 -0500

    validacion en reserva

--- comun/comun-dominio/src/main/java/com/ceiba/dominio/constantes/ConstantesUtil.java
+++ comun/comun-dominio/src/main/java/com/ceiba/dominio/constantes/ConstantesUtil.java
@@ -6,6 +6,7 @@ public class ConstantesUtil {
 	public static final String SE_DEBE_INGRESAR_EL_PRECIO = "Se debe ingresar el precio del producto";
 	public static final String SE_DEBE_INGRESAR_EL_TIPO_PRODUCTO = "Se debe ingresar el tipo del producto";
 	public static final String SE_DEBE_INGRESAR_EL_NOMBRE_DE_PRODUCTO = "Se debe ingresar el nombre de producto";
+	public static final String SE_DEBE_INGRESAR_LA_FECHA_DE_RESERVA = "Se debe ingresar la fecha de la reserva";
 	public static final String SE_DEBE_INGRESAR_UN_VALOR_POSITIVO = "Se debe ingresar un valor positivo";
 	public static final String EL_PRODUCTO_YA_EXISTE_EN_EL_SISTEMA = "El producto ya existe en el sistema";
 	public static final String EL_PRODUCTO_NO_EXISTE_EN_EL_SISTEMA = "El producto no existe en el sistema";
--- microservicio/dominio/src/main/java/com/ceiba/reserva/modelo/entidad/Reserva.java
+++ microservicio/dominio/src/main/java/com/ceiba/reserva/modelo/entidad/Reserva.java
@@ -3,6 +3,9 @@ package com.ceiba.reserva.modelo.entidad;
 import lombok.Getter;
 import lombok.Setter;
 
+import static com.ceiba.dominio.ValidadorArgumento.validarObligatorio;
+import static com.ceiba.dominio.constantes.ConstantesUtil.*;
+
 import java.time.LocalDate;
 
 @Getter
@@ -14,6 +17,7 @@ public class Reserva {
 	private Long idProducto;
 
 	public Reserva(Long id, LocalDate fechaReserva, Long idProducto) {
+		validarObligatorio(fechaReserva, SE_DEBE_INGRESAR_LA_FECHA_DE_RESERVA);
 		this.id = id;
 		this.fechaReserva = fechaReserva;
 		this.idProducto = idProducto;
